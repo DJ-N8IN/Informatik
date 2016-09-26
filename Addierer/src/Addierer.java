42ba2a750a8271d51cbdc147be211ed0cb4efa53
Lipp.Jan <Lipp.Jan@SK-D43D7EB3B863.2631.skool.net>
2016-09-26 15:26:15 +0200

    Initial commit

---
+++ Addierer/src/Addierer.java
@@ -0,0 +1,24 @@
+
+public class Addierer {
+
+		static int number=1;
+		
+	public static void main(String[] args) {
+		// TODO 
+		System.out.println(addiere(2,1));
+		System.out.println(mult(4,3));
+		System.out.println(div(12,3));
+	}
+
+	public static int addiere(int zahl1, int zahl2){
+		return zahl1+zahl2;}
+	
+	
+	public static int mult(int zahl1, int zahl2){
+		return zahl1*zahl2;}
+	
+	
+	public static int div(int zahl1, int zahl2){
+		return zahl1/zahl2;}
+	}
+
